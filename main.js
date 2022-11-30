1b932b5a791a5a32c617f7c876a76a2b300e1770
CaroGomezCala <lcgk.021@gmail.com>
2022-11-29 20:34:26 -0500

    estrictamente_igual

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 addEventListener("DOMContentLoaded", (e)=>{
     //Operadores de comparacion
-    //No es Igual: Devuelve un valor booleano true si los operandos no son iguales.
-    let num1 = prompt("Digita un numero por favor: ");
-    let num2 = prompt("Digita un segundo numero por favor: ");
+    //Estrictamente Igual: Devuelve un valor booleano true si los operandos son del mismo tipom.
+    let num1 = Number(prompt("Digita un numero por favor: "));
+    let num2 = Number(prompt("Digita un segundo numero por favor: "));
     console.log(`Dados los numeros ${num1} y ${num2}`);
-    console.log(`El Numero 1 y Numero 2 no son iguales?. True = verdadero, False = Falso`);
-    console.log(num1==num2);
-    console.log(`El Numero 1 no es igual a 5?. True = verdadero, False = Falso`);
-    console.log(5!=num1);
+    console.log(`El Numero 1 y Numero 2 son estrictamente iguales?. True = verdadero, False = Falso`);
+    console.log(num1===num2);
+    console.log(`El Numero 1 es estrictamente igual a 5?. True = verdadero, False = Falso`);
+    console.log(num1===5);
 })
