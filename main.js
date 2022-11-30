cd5b62f9873fc6a9ddb715308a76cc58ca94a752
CaroGomezCala <lcgk.021@gmail.com>
2022-11-29 20:09:07 -0500

    operador_igual

---
+++ main.js
@@ -0,0 +1,9 @@
+addEventListener("DOMContentLoaded", (e)=>{
+    //Operadores de comparacion
+    //Igual: Devuelve un valor booleano true si los operandos son iguales.
+    let num1 = prompt("Digita un numero por favor: ");
+    let num2 = prompt("Digita un segundo numero por favor: ");
+    console.log(`Dados los numeros ${num1} y ${num2}`);
+    console.log(`Son el Numero 1 y Numero 2 iguales?. True = verdadero, False = Falso`);
+    console.log(num1==num2);
+})
