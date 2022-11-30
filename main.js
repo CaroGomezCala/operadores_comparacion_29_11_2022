1bc59773eb402929a68b4fdb3e01b1e9ba282439
CaroGomezCala <lcgk.021@gmail.com>
2022-11-29 20:17:28 -0500

    operador_no_es_igual

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 addEventListener("DOMContentLoaded", (e)=>{
     //Operadores de comparacion
-    //Igual: Devuelve un valor booleano true si los operandos son iguales.
+    //No es Igual: Devuelve un valor booleano true si los operandos no son iguales.
     let num1 = prompt("Digita un numero por favor: ");
     let num2 = prompt("Digita un segundo numero por favor: ");
     console.log(`Dados los numeros ${num1} y ${num2}`);
-    console.log(`Son el Numero 1 y Numero 2 iguales?. True = verdadero, False = Falso`);
+    console.log(`El Numero 1 y Numero 2 no son iguales?. True = verdadero, False = Falso`);
     console.log(num1==num2);
+    console.log(`El Numero 1 no es igual a 5?. True = verdadero, False = Falso`);
+    console.log(5!=num1);
 })
