b0e8817fa68bace9a2985ca4a013f938920d26ec
CaroGomezCala <lcgk.021@gmail.com>
2022-11-29 21:08:50 -0500

    mayor_igual_que

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 addEventListener("DOMContentLoaded", (e)=>{
     /*Operadores de comparacion
-    Mayor que: devuelve valor boolean true si el operando izquierdo es mayor que el derecho.
+    Mayor o igual que: devuelve valor boolean true si el operando izquierdo es mayor o igual que el derecho.
     */
     let num1 = Number(prompt("Digita un numero por favor: "));
     let num2 = Number(prompt("Digita un segundo numero por favor: "));
-    console.log(`Es el numero ${num1} mayor que el numero ${num2}?`);
-    console.log(num1>num2);
+    console.log(`Es el numero ${num1} mayor o igual que el numero ${num2}?`);
+    console.log(num1>=num2);
 })
